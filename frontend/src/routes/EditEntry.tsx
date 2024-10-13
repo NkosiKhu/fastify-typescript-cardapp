5a2adf824fedae7312ad59f1d8db4f19f29eb482
Nkosi Khumalo <nkosik11@gmail.com>
2024-10-13 19:52:22 +0100

    dark mode styling

--- frontend/src/routes/EditEntry.tsx
+++ frontend/src/routes/EditEntry.tsx
@@ -1,34 +1,34 @@
-import {useState, useContext, ChangeEvent, MouseEvent, useEffect} from 'react'
-import {useParams, useNavigate} from 'react-router-dom'
-import {EntryContext} from '../utilities/globalContext'
-import {Entry, EntryContextType} from '../@types/context'
+import { useState, useContext, ChangeEvent, MouseEvent, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { EntryContext } from '../utilities/globalContext'
+import { Entry, EntryContextType } from '../@types/context'
 
-export default function EditEntry(){
-    const {id} = useParams()
-    const emptyEntry: Entry = {title: "", description: "",created_at: new Date()}
+export default function EditEntry() {
+    const { id } = useParams()
+    const emptyEntry: Entry = { title: "", description: "", created_at: new Date() }
 
     const { updateEntry, entries } = useContext(EntryContext) as EntryContextType
-    const [newEntry,setNewEntry] = useState<Entry>(emptyEntry)
+    const [newEntry, setNewEntry] = useState<Entry>(emptyEntry)
 
-    useEffect(() =>{
-        const entry = entries.filter(entry=> entry.id == id)[0]
+    useEffect(() => {
+        const entry = entries.filter(entry => entry.id == id)[0]
         setNewEntry(entry)
-    },[])
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+    }, [])
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setNewEntry({
             ...newEntry,
-            [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         })
     }
     const handleSend = (e: MouseEvent<HTMLButtonElement>) => {
-        updateEntry(id as string,newEntry)
+        updateEntry(id as string, newEntry)
     }
-    return(
-        <section className="flex justify-center flex-col w-fit ml-auto mr-auto mt-10 gap-5 bg-gray-300 p-8 rounded-md">
-            <input className="p-3 rounded-md" type="text" placeholder="Title" name="title" value={newEntry.title} onChange={handleInputChange}/>
-            <textarea className="p-3 rounded-md" placeholder="Description" name="description" value={newEntry.description} onChange={handleInputChange}/>
-            <input className="p-3 rounded-md" type="date" name="created_at" value={(new Date(newEntry.created_at)).toISOString().split('T')[0]} onChange={handleInputChange}/>
-            <button onClick={(e) => {handleSend(e)}} className="bg-blue-400 hover:bg-blue-600 font-semibold text-white p-3 rounded-md">Update</button>
+    return (
+        <section className="flex justify-center flex-col w-fit ml-auto mr-auto mt-10 gap-5 bg-gray-300 dark:bg-gray-700 p-8 rounded-md">
+            <input className="p-3 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white" type="text" placeholder="Title" name="title" value={newEntry.title} onChange={handleInputChange} />
+            <textarea className="p-3 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white" placeholder="Description" name="description" value={newEntry.description} onChange={handleInputChange} />
+            <input className="p-3 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white" type="date" name="created_at" value={(new Date(newEntry.created_at)).toISOString().split('T')[0]} onChange={handleInputChange} />
+            <button onClick={(e) => { handleSend(e) }} className="bg-blue-400 hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-900 font-semibold text-white p-3 rounded-md">Update</button>
         </section>
     )
 }
